fc3acda00ac103d2577d76e667b4b2c2bf82ea85
ClockworkPirate <mguerrieri@outlook.com>
2015-09-07 00:17:08 -0700

    Some garbage

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var rolls = [5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11].reverse();
+var rolls = [5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11];
 
 var catanBoard = function (width) {
 	var board = [];
@@ -8,7 +8,7 @@ var catanBoard = function (width) {
 		var row = [];
 		var rowLen = width - Math.abs(width / 2 - i) - ((i > width / 2) ? 1 : 0);
 		for (var j = 0; j < rowLen; j ++) {
-			row.push(pastRowsLen + j == desertLoc ? -1 : rolls.pop());
+			row.push(pastRowsLen + j == desertLoc ? -1 : rolls.shift());
 		}
 		board.push(row);
 		pastRowsLen += rowLen;
@@ -16,8 +16,113 @@ var catanBoard = function (width) {
 	console.log(board);
 }
 
+var SQRT_3 = Math.sqrt(3);
+
+var makeNiceContext = function (ctx) {
+	ctx.x = 0;
+	ctx.y = 0;
+	
+	ctx.move = function (x, y) {
+		this.x = x;
+		this.y = y;
+		return this.moveTo(x, y);
+	}
+	
+	ctx.moveR = function (dx, dy) {
+		this.x += dx;
+		this.y += dy;
+		return this.moveTo(this.x, this.y);
+	}
+	
+	ctx.lineR = function (dx, dy) {
+		this.x += dx;
+		this.y += dy;
+		return this.lineTo(this.x, this.y);
+	}
+
+	ctx.drawRegHex = function (sideLength) {
+		this.moveR(0, -sideLength);
+		this.lineR(SQRT_3 * sideLength / 2, sideLength / 2);
+		this.lineR(0, sideLength);
+		this.lineR(SQRT_3 * -sideLength / 2, sideLength / 2);
+		this.lineR(SQRT_3 * -sideLength / 2, -sideLength / 2);
+		this.lineR(0, -sideLength);
+		this.lineR(SQRT_3 * sideLength / 2, -sideLength / 2);
+		this.moveR(0, sideLength);
+	}
+	
+	ctx.moveHexCenter = function (sideLength, dx, dy) {
+		this.moveR(SQRT_3 * sideLength * (dx - (dy % 2 == 0 ? 0 : 0.5)), 1.5 * sideLength * dy);
+	}
+	
+	return ctx;
+}
+
 var main = function () {
-	console.log($("canvas#board"));
+	var ctx = makeNiceContext($("canvas#board")[0].getContext("2d"));
+	var sideLength = 50;
+	ctx.beginPath();
+	ctx.move(300, 100);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, -2, 1);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, -3, 1);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, -4, 1);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, -4, 1);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, -3, 1);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, -2, 1);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.moveHexCenter(sideLength, 1, 0);
+	ctx.drawRegHex(sideLength);
+	ctx.stroke();
 }
 
 $(document).ready(main);
