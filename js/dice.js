3834503acb3521a8f711866e8716ea3d67994c88
Mario Guerrieri <mguerrieri@outlook.com>
2015-09-07 04:19:05 -0700

    more stuff

--- js/dice.js
+++ js/dice.js
@@ -1,6 +1,11 @@
 var main = function () {
+	var red = document.createElement("div");
+	$(this).append(red);
+	
+	var yellow = document.createElement("div");
+	$(this).append(yellow);
+	
 	$("div#dice").click(function () {
-		$(this).add(new HTMLDivElement())
 	});
 };
 
